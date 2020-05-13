56b923b7b9ff0beb9b497ee130097523be9598e6
jeremy44144@gmail.com <jeremy44144@gmail.com>
2020-05-13 20:14:00 +0800

    bug fixed

--- js/home.js
+++ js/home.js
@@ -139,7 +139,19 @@ $(function() {
 });
 var index = 0;
 var timer; //定时器
-//定时器 播放
+
+// ========================文字滚动===============================
+var box=document.getElementById("Marquee"),can=true;
+box.innerHTML+=box.innerHTML;
+box.onmouseover=function(){can=false};
+box.onmouseout=function(){can=true};
+new function (){
+var stop=box.scrollTop%40==0&&!can;
+if(!stop)box.scrollTop==parseInt(box.scrollHeight/2)?box.scrollTop=0:box.scrollTop++;
+setTimeout(arguments.callee,box.scrollTop%40?10:1500);
+};
+// ========================END===============================
+
 //初始化
 function init() {
     var len = $('.banList li').length; //获取图片有多少张
